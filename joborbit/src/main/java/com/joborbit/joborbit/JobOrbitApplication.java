b49f605b1ca1b96f32c1bbcb4b540ea44dac6473
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-07 19:50:45 +0530

    Spring IO

---
+++ joborbit/src/main/java/com/joborbit/joborbit/JobOrbitApplication.java
@@ -0,0 +1,13 @@
+package com.joborbit.joborbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOrbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOrbitApplication.class, args);
+	}
+
+}
---
+++ joborbit/src/test/java/com/joborbit/joborbit/JobOrbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joborbit.joborbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOrbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
