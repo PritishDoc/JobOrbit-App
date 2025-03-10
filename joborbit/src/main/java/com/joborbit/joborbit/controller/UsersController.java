800613cd04d18cfa088d06c50051ad867a6c2a0a
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-10 15:04:54 +0530

    Update UsersController.java

--- joborbit/src/main/java/com/joborbit/joborbit/controller/UsersController.java
+++ joborbit/src/main/java/com/joborbit/joborbit/controller/UsersController.java
@@ -3,9 +3,12 @@ package com.joborbit.joborbit.controller;
 import com.joborbit.joborbit.entity.Users;
 import com.joborbit.joborbit.entity.UsersType;
 import com.joborbit.joborbit.services.UserTypeService;
+import jakarta.validation.Valid;
+import org.apache.catalina.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,5 +27,11 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register"; // Fixed return statement
+    }
+    @PostMapping("/register/new")
+    public String userRegistation(@Valid User users){
+        System.out.println("User:: "+users);
+        return "dashboard";
+
     }
 }
