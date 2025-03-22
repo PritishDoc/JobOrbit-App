65597d5b2c72b4b9e9a27c6708cd5c69a66c83ea
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-22 13:19:17 +0530

    Delete joborbit directory

--- joborbit/src/main/java/com/joborbit/joborbit/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joborbit.joborbit.repository;
-
-import com.joborbit.joborbit.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<Users, Integer> {
-    List<Users> findByEmail(String email);  // Returns list to handle multiple results
-}
--- joborbit/src/main/java/com/joborbit/joborbit/repository/UserTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.joborbit.joborbit.repository;
-
-import com.joborbit.joborbit.entity.UsersType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTypeRepository extends JpaRepository<UsersType,Integer> {
-}
