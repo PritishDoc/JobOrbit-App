463f87990e44ad58f35de0b6fadd6c4b96dd7ede
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-03-22 13:22:53 +0530

    new file added

---
+++ job-orbit/job-orbit/src/test/java/com/joborbit/jobportal/JobOrbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joborbit.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOrbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- joborbit/src/main/java/com/joborbit/joborbit/repository/JobSeekerProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.joborbit.joborbit.repository;
-
-import com.joborbit.joborbit.entity.JobSeekerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile,Integer> {
-}
