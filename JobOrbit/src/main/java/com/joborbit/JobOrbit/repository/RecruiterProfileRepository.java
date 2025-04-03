a8015f98fb9825765192e7c17e350f5270aeed05
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-03 16:34:25 +0530

    Create RecruiterProfileRepository.java

---
+++ JobOrbit/src/main/java/com/joborbit/JobOrbit/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.joborbit.JobOrbit.repository;
+
+import com.joborbit.JobOrbit.Entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile,Integer> {
+}
