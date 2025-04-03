677f56d6ff12bd21541c9982bf45281bee255c70
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-03 22:51:41 +0530

    Update WebSecurityConfig.java

--- JobOrbit/src/main/java/com/joborbit/JobOrbit/config/WebSecurityConfig.java
+++ JobOrbit/src/main/java/com/joborbit/JobOrbit/config/WebSecurityConfig.java
@@ -1,8 +1,9 @@
 package com.joborbit.JobOrbit.config;
 
-package com.luv2code.jobportal.config;
 
-import com.luv2code.jobportal.services.CustomUserDetailsService;
+
+
+import com.joborbit.JobOrbit.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
