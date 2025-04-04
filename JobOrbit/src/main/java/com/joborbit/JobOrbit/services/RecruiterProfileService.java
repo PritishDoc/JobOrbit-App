a34dc728514549fa8bd86e3a02f52ce899baf674
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-04 14:52:47 +0530

    Create RecruiterProfileService.java

---
+++ JobOrbit/src/main/java/com/joborbit/JobOrbit/services/RecruiterProfileService.java
@@ -0,0 +1,27 @@
+package com.joborbit.JobOrbit.services;
+
+import com.joborbit.JobOrbit.Entity.RecruiterProfile;
+import com.joborbit.JobOrbit.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+
+    private final RecruiterProfileRepository recruiterRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterRepository) {
+        this.recruiterRepository = recruiterRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id) {
+        return recruiterRepository.findById(id);
+    }
+
+    public RecruiterProfile addNew(RecruiterProfile recruiterProfile) {
+        return recruiterRepository.save(recruiterProfile);
+    }
+}
