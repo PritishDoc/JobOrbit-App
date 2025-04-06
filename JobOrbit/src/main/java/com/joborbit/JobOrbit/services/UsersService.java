a27aeaa12ab7a4307cad160d192d10185cbda0a0
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-06 22:07:47 +0530

    Update UsersService.java

--- JobOrbit/src/main/java/com/joborbit/JobOrbit/services/UsersService.java
+++ JobOrbit/src/main/java/com/joborbit/JobOrbit/services/UsersService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import java.util.Date;
 import java.util.Optional;
 
@@ -69,6 +70,19 @@ public class UsersService {
 
         return null;
     }
+
+    public Users getCurrentUser() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String username = authentication.getName();
+            Users user = usersRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Could not found " + "user"));
+            return user;
+        }
+
+        return null;
+    }
+
 
     public Optional<Users> getUserByEmail(String email) {
         return usersRepository.findByEmail(email);
