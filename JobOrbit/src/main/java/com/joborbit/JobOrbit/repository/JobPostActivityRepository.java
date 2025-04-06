c98d1ad52a9269cd35d84e648688654b61098136
Pritish kumar Ray <141579651+PritishDoc@users.noreply.github.com>
2025-04-06 22:07:37 +0530

    activity

---
+++ JobOrbit/src/main/java/com/joborbit/JobOrbit/repository/JobPostActivityRepository.java
@@ -0,0 +1,7 @@
+package com.joborbit.JobOrbit.repository;
+
+import com.joborbit.JobOrbit.Entity.JobPostActivity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobPostActivityRepository extends JpaRepository<JobPostActivity, Integer> {
+}
